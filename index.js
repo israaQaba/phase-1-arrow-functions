e720b4e21851b6fed063a5e1bd804509a8695aaa
Israa Qaba <israa.qaba98@gmail.com>
2022-04-27 05:03:09 +0300

    Done

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = () => 2000/100;
+console.log(divide());
+
+//this one the test doesn't pass it
+const divideMine = (a,b) => a/b;
+console.log(divide(2000, 100));
+
+
+const square = (num) => num*num;
+console.log(square(2));
+
+
+const add = (a,b) => a+b;
+console.log(add(3,4));
